5d357b69c3b72c631bbb237003327b2d989bd307
Marcin Wrzesiński <mwrzesinski@taxus.com.pl>
2015-01-04 11:22:15 +0100

    ggg

--- git/src/git/GitTest.java
+++ git/src/git/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("Git git git");
 		System.out.println("Git git git");
+		System.out.println("Git git git");
 	}
 
 }
