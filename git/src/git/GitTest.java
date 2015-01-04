4bcc2a83b02288084cb7f0ab95e5a0f8e67b57f5
Marcin Wrzesiński <mwrzesinski@taxus.com.pl>
2015-01-04 12:54:05 +0100

    tako

--- git/src/git/GitTest.java
+++ git/src/git/GitTest.java
@@ -5,7 +5,9 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("Git git git");
 		System.out.println("Git git git");
-		System.out.println("Git git git");
+		System.out.println("Git gitsd git");
+
+		System.out.println("Wiele bólu mam, wiele ninawiœci te¿.");
 	}
 
 }
